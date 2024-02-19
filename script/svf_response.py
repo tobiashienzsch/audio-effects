393cabbe26c66d5d5fa911b2baa8a303173f544d
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-02-20 00:54:20 +0100

    [frequency] Add Frequency Crusher effect

--- script/svf_response.py
+++ script/svf_response.py
@@ -15,21 +15,9 @@ plt.semilogx(f/(np.pi*2), 20*np.log10(np.abs(H05)), label="ω = 0.5")
 plt.semilogx(f/(np.pi*2), 20*np.log10(np.abs(H10)), label="ω = 1.0")
 plt.semilogx(f/(np.pi*2), 20*np.log10(np.abs(H11)), label="ω = 1.1")
 
-# plt.semilogx(f/(np.pi*2), np.angle(H01), label="ω = 0.1")
-# plt.semilogx(f/(np.pi*2), np.angle(H02), label="ω = 0.2")
-# plt.semilogx(f/(np.pi*2), np.angle(H05), label="ω = 0.5")
-# plt.semilogx(f/(np.pi*2), np.angle(H10), label="ω = 1.0")
-# plt.semilogx(f/(np.pi*2), np.angle(H11), label="ω = 1.1")
-
-# plt.vlines(0.02*np.pi,-25,0)
-# plt.vlines(0.1*np.pi,-25,0)
-# plt.vlines(np.pi,-25,0)
-
-plt.legend()
 plt.grid(which="major", linewidth=1)
 plt.grid(which="minor", linewidth=0.2)
 plt.xticks([0.1, 0.5], [0.1, 0.5])
 plt.minorticks_on()
+plt.legend()
 plt.show()
-
-# print(np.abs(H))
